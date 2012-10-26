5ebf01f59b9b989d6d038143892c3b8e0dd55cbb
Luis Román <luisromangz@gmail.com>
2012-10-26 11:11:22 +0100

    * Added legal notice to source files. * Added recursion when processing an element, so fields in its base classes can recursively be writen to the DXF file.

--- Dexefer/src/org/dexefer/DXFFile.java
+++ Dexefer/src/org/dexefer/DXFFile.java
@@ -1,3 +1,19 @@
+/*
+   Copyright 2012 Luis Román Gutiérrez
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
 package org.dexefer;
 
 import java.io.IOException;
@@ -5,7 +21,6 @@ import java.io.OutputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Iterator;
 
 import org.dexefer.annotations.DXFElementType;
 import org.dexefer.annotations.DXFProperty;
@@ -57,8 +72,28 @@ public class DXFFile {
 			writer.writeEntry(2, className);
 		}	
 		
+		writeElementFields(writer, element, eClass);
+		
+		DXFTerminator terminatorA = eClass.getAnnotation(DXFTerminator.class);
+		if(terminatorA!=null) {
+			// We write the element's terminator
+			writer.writeEntry(0, terminatorA.value());
+		}
+	}
+	
+	private void writeElementFields(
+			DXFWriter writer, 
+			DXFElement element,
+			Class<? extends DXFElement> eClass) 
+					throws IOException {
+		
+		if(DXFElement.class.isAssignableFrom(eClass.getSuperclass())) {
+			// If the parent class is a DXFElement too, we add the fields declared there first.
+			writeElementFields(writer, element, (Class<? extends DXFElement>) eClass.getSuperclass());
+		}
+		
 		// We iterate the fields.
-		for(Field f : element.getClass().getDeclaredFields()) {
+		for(Field f : eClass.getDeclaredFields()) {
 			
 			DXFProperty propertyA = f.getAnnotation(DXFProperty.class);
 			DXFSubElements subElementsA = f.getAnnotation(DXFSubElements.class);
@@ -68,13 +103,13 @@ public class DXFFile {
 				throw new IllegalStateException(String.format("Field '%s' in class '%s' cannot have annotations DXFProperty and DXFSubElements at the same time."));				
 			} else  if(propertyA!=null) {
 				// We write the property's value, if it's not null (optional).
-				Object value =  getFieldValue(f, element);
+				Object value =  getFieldValue(eClass,f, element);
 				if(value!=null){
 					int code = propertyA.value();
 					writer.writeEntry(code,value.toString());
 				}
 			} else if (subElementsA!=null) {				
-				Object collection = getFieldValue(f, element);
+				Object collection = getFieldValue(eClass,f,  element);
 				if(!Iterable.class.isInstance(collection)){
 					throw new IllegalStateException(String.format("The field '%s' is not Iterable so it cannot use the @DXFSubElements annotation."));
 				}
@@ -83,16 +118,10 @@ public class DXFFile {
 					writeElement(writer, subElement);
 				}
 			}
-		}
-		
-		DXFTerminator terminatorA = eClass.getAnnotation(DXFTerminator.class);
-		if(terminatorA!=null) {
-			// We write the element's terminator
-			writer.writeEntry(0, terminatorA.value());
-		}
+		}		
 	}
-	
-	private Object getFieldValue(Field field, DXFElement element) {
+
+	private Object getFieldValue(Class<? extends DXFElement> eClass, Field field,  DXFElement element) {
 		
 		try {
 			return field.get(element);
@@ -107,7 +136,7 @@ public class DXFFile {
 		String getterName = prefix+ field.getName().substring(0, 1).toUpperCase()+field.getName().substring(1);
 		Method method;
 		try{
-			method = element.getClass().getMethod(getterName);
+			method = eClass.getMethod(getterName);
 		} catch(NoSuchMethodException e) {
 			throw new IllegalStateException(
 					String.format("Field '%s' is not public and has no getter method.", field.getName()), e);
--- Dexefer/src/org/dexefer/entities/Entity.java
+++ Dexefer/src/org/dexefer/entities/Entity.java
@@ -1,3 +1,18 @@
+/*
+   Copyright 2012 Luis Román Gutiérrez
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
 package org.dexefer.entities;
 
 import org.dexefer.DXFElement;
@@ -10,18 +25,63 @@ import org.dexefer.annotations.DXFProperty;
  */
 public abstract class Entity implements DXFElement {
 	@DXFProperty(5)
+	private
 	Integer handle;
 	
 	@DXFProperty(8)
+	private
 	String layerName;
 	
 	@DXFProperty(6)
+	private
 	String lineTypeName = "BYLAYER";
 	
 	@DXFProperty(62)
+	private
 	String colorNumber = "BYLAYER";
 	
 	@DXFProperty(48)
+	private
 	Double lineTypeScale = 1.0;
+
+	public Integer getHandle() {
+		return handle;
+	}
+
+	public void setHandle(Integer handle) {
+		this.handle = handle;
+	}
+
+	public String getLayerName() {
+		return layerName;
+	}
+
+	public void setLayerName(String layerName) {
+		this.layerName = layerName;
+	}
+
+	public String getLineTypeName() {
+		return lineTypeName;
+	}
+
+	public void setLineTypeName(String lineTypeName) {
+		this.lineTypeName = lineTypeName;
+	}
+
+	public String getColorNumber() {
+		return colorNumber;
+	}
+
+	public void setColorNumber(String colorNumber) {
+		this.colorNumber = colorNumber;
+	}
+
+	public Double getLineTypeScale() {
+		return lineTypeScale;
+	}
+
+	public void setLineTypeScale(Double lineTypeScale) {
+		this.lineTypeScale = lineTypeScale;
+	}
 
 }
